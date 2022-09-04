7569fad9c09469c420a52031dd35808388353399
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-09-04 10:34:15 -0300

    inicio

---
+++ Greys Anatomy/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    private Vector2 lastClikedPos;
+    private bool moving;
+    void Update()
+    {
+        
+    }
+}
