ab9f31fc804b4a2f379ee0b9d6d88e689dae3f0e
Fernando Souza <fernando.bentomouradesouza@gmail.com>
2022-09-04 10:54:24 -0300

    movimentacao

--- Greys Anatomy/Assets/Scripts/PlayerMovement.cs
+++ Greys Anatomy/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float movementSpeed = 5f;
     private Vector2 lastClikedPos;
     private bool moving;
+
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastClikedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            moving = true;
+        }
+
+        if(moving && (Vector2)transform.position != lastClikedPos)
+        {
+            float step = movementSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, lastClikedPos, step);
+        }
+        else
+        {
+            moving = false;
+        }
     }
 }
